d76a11dec9a92cbf7bd0f0d82c0510b08209552e
tolip05 <kosta1980@abv.bg>
2018-12-18 15:26:05 +0200

    commit

---
+++ TheTankGame/Core/Engine.cs
@@ -0,0 +1,57 @@
+﻿namespace TheTankGame.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+    using IO.Contracts;
+
+    public class Engine : IEngine
+    {
+        private bool isRunning;
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(
+            IReader reader, 
+            IWriter writer, 
+            ICommandInterpreter commandInterpreter)
+        {
+            this.reader = reader;
+            this.writer = writer;
+            this.commandInterpreter = commandInterpreter;
+
+            this.isRunning = false;
+        }
+
+        public void Run()
+        {
+            while (!isRunning)
+            {
+                try
+                {
+                    string input = reader.ReadLine();
+                    if (input == "Terminate ")
+                    {
+                        isRunning = true;
+                        
+                    }
+                    List<string> data = input.Split().ToList();
+
+                    string result = this.commandInterpreter.ProcessInput(data);
+
+                    writer.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    writer.WriteLine(e.Message);
+                }
+              
+            }
+            Environment.Exit(0);
+
+
+        }
+    }
+}
---
+++ TheTankGame/Entities/Vehicles/Factories/VehicleFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TheTankGame.Entities.Miscellaneous.Contracts;
+using TheTankGame.Entities.Vehicles.Contracts;
+using TheTankGame.Entities.Vehicles.Factories.Contracts;
+
+namespace TheTankGame.Entities.Vehicles.Factories
+{
+    public class VehicleFactory : IVehicleFactory
+    {
+        public IVehicle CreateVehicle(string vehicleType, string model, double weight, decimal price, int attack, int defense, int hitPoints)
+        {
+            string assembler = "VehicleAssembler";
+            Type typeAssembler = Assembly.GetCallingAssembly().GetTypes()
+                .FirstOrDefault(t => t.Name == assembler);
+            IAssembler assemblerClass = (IAssembler)Activator.CreateInstance(typeAssembler);
+
+            Type types = Assembly.GetCallingAssembly().GetTypes()
+                .FirstOrDefault(t => t.Name == vehicleType);
+            IVehicle vehicle = (IVehicle)Activator.CreateInstance(types, new object[]{
+                 model,
+                 weight,
+                 price,
+                 attack,
+                 defense,
+                 hitPoints,
+                 assemblerClass
+            });
+            return vehicle;
+        }
+    }
+}
